f5a73e009056892e6aa94a05d26e92ba0f3eef97
ayoubha10 <43857680+ayoubha10@users.noreply.github.com>
2018-10-19 13:19:19 +0200

    Add files via upload
    menu con opcion 1 implementada casi

--- Menú.sh
+++ Menú.sh
@@ -30,6 +30,16 @@ case "$opt" in
         tLen=${#arrId[@]}
         numRest=0
         totalNoVino=0
+        puntuacio=()
+        tp=0
+        Millor_p=0
+        Millor=0
+
+        for ((j=0; j<=130; j++))
+        do
+          puntuacio[j]=0
+
+        done
         for (( i=0; i<$tLen; i++ ))
         do
           arrRating=($(cat rating_final.csv | grep ${arrId[i]}))
@@ -48,19 +58,57 @@ case "$opt" in
           tmp=$(($tmp/$tLen1))
           if [ $tmp -ge 1 ]
           then
-            echo $tmp:${arrId[i]}
-            echo
+            echo Id_Rest: ${arrId[i]}
+
             puntAct=0
             aux=($(cat geoplaces2.csv | grep ${arrId[i]} | cut -d  "," -f12))
             if [[ $aux =~ .*'Wine-Beer'.* ]]
             then
-              totalNoVino=$(($totalNoVino+1))
+              puntuacio[i]=$((${puntuacio[i]} + 2))
+
             fi
-            
+            aux1=($(cat geoplaces2.csv | grep ${arrId[i]} | cut -d  "," -f13))
+            if [[ $aux1  =~ .*'none'.* ]] || [[$aux1  =~ .*'not-permitted'.* ]]
+            then
+
+              puntuacio[i]=$((${puntuacio[i]} + 1))
+
+            fi
+            aux2=($(cat geoplaces2.csv | grep ${arrId[i]} | cut -d  "," -f14))
+            if [[ $aux2  =~ .*'informal'.* ]] || [[$aux2  =~ .*'casual'.*]]
+            then
+
+              puntuacio[i]=$((${puntuacio[i]} + 1))
+
+            fi
+            aux3=($(cat geoplaces2.csv | grep ${arrId[i]} | cut -d  "," -f15))
+            if [[ $aux3  =~ .*'partially'.* ]] || [[$aux3  =~ .*'completely'.*]]
+            then
+
+              puntuacio[i]=$((${puntuacio[i]} + 2))
+
+            fi
+            aux4=($(cat geoplaces2.csv | grep ${arrId[i]} | cut -d  "," -f16))
+            if [[ $aux4 =~ .*'medium'.* ]]
+            then
+              puntuacio[i]=$((${puntuacio[i]} + 4))
+
+            fi
+            echo Puntuacio_Rest: ${puntuacio[i]}
+            echo
+            echo
+            echo
+            if [[ ${puntuacio[i]} -gt  $millor_p ]]
+            then
+            millor_p=${puntuacio[i]}
+            Millor=${arrId[i]}
+            echo tes
+            fi
           fi
 
-          numRest=$((1+$numRest))
+          numRest=$((1 + $numRest))
         done
+        echo $Millor, $millor_p
         echo Numero De Restaurantes:$numRest
         echo Total Sin Vino:$totalNoVino
         #tot=0
